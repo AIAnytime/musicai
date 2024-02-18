9448a64621792ec438bb69e97d1ca752b7fddd0f
AI Anytime <sonu1000raw@gmail.com>
2024-02-19 00:10:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,24 +49,8 @@ async def index(request: Request):
 @app.post("/generate-music")
 async def generate_music(prompt: str = Form(...), duration: int = Form(...)):
     lyrics = generate_lyrics(prompt)
-    prompt_with_lyrics = f"♪ {lyrics} ♪"
-    # output = replicate.run(
-    #     "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
-    #     input={
-    #         "top_k": 250,
-    #         "top_p": 0,
-    #         "prompt": prompt,
-    #         "duration": duration,
-    #         "temperature": 1,
-    #         "continuation": False,
-    #         "model_version": "stereo-melody-large",
-    #         "output_format": "wav",
-    #         "continuation_start": 0,
-    #         "multi_band_diffusion": False,
-    #         "normalization_strategy": "peak",
-    #         "classifier_free_guidance": 3
-    #     }
-    # )
+    prompt_with_lyrics = lyrics
+    print(prompt_with_lyrics)
     output = replicate.run(
         "suno-ai/bark:b76242b40d67c76ab6742e987628a2a9ac019e11d56ab96c4e91ce03b79b2787",
         input={
@@ -76,6 +60,9 @@ async def generate_music(prompt: str = Form(...), duration: int = Form(...)):
             "waveform_temp": 0.7
         }
     )
-    music_path_or_url = output
+    print(output)
+    music_url = output['audio_out']
+    music_path_or_url = music_url
+    
+    print(music_path_or_url)
     return JSONResponse(content={"url": music_path_or_url})
-
